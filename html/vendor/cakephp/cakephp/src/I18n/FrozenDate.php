af4aa75ca57c7ce7e92e86e0d44d31dbae796fa9
mpxvm <maran.emil@gmail.com>
2025-01-17 09:59:06 +0100

    howto

--- html/vendor/cakephp/cakephp/src/I18n/FrozenDate.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-use function Cake\Core\deprecationWarning;
-
-deprecationWarning('5.0.0', 'Cake\I18n\FrozenDate is deprecated. Use Cake\I18n\Date instead');
-
-class_exists('Cake\I18n\Date');
--- html/vendor/cakephp/migrations/docs/en/contents.rst
+++
@@ -1,6 +0,0 @@
-.. toctree::
-    :maxdepth: 2
-    :caption: CakePHP Migrations
-
-    /index
-    /upgrading-to-builtin-backend
