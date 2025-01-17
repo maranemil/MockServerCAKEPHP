24cc3621033717b9003a5cff5eacc7e8991f82ac
maranemil <maran.emil@gmail.com>
2025-01-17 09:36:37 +0100

    howto

--- test.sh
+++ test.sh
@@ -5,5 +5,6 @@ URL=http://localhost:8082/echo/index
 URL=http://localhost:8082/echo/get-status
 URL=http://localhost:8081/index.php/echo
 
+
 #curl  $URL
 curl  -X POST --header "Content-Type: application/json"  $URL
